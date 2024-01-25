2ea0899c2deb20fa7ac1b237ccd2eb7fb314e0b8
Rohit Saini <emailrohitsaini@gmail.com>
2024-01-25 21:58:19 +0530

    Fixed logo

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
+// Logo.js
 import React from "react";
 import LogoImage from "../../assets/logo.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
-    <>
+    <div className="logo-container">
       <img src={LogoImage} alt="Logo" width={67} height={34} />
-    </>
+    </div>
   );
 };
 
