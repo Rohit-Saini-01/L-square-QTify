20af82ce7645b56e897b35731e27cb3735aeb787
Rohit Saini <emailrohitsaini@gmail.com>
2024-01-27 17:38:55 +0530

    changes

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Button from "../Button/Button";
 import Search from "../Search/Search";
-import styles from './Navbar.module.css'
-import LogoImage from "../../assets/Logo.png"
+import styles from "./Navbar.module.css";
+// import LogoImage from "../../assets/Logo.png"
 
-
-export default function Navbar(){
-    return(
-        <>
-            <nav className={styles.navbar}>
-                <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
-                 <Search search={"Search a song of your choice"}/>
-                <Button children="Give Feedback"/>
-            </nav>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        {/* <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div> */}
+        <Search search={"Search a song of your choice"} />
+        <Button children="Give Feedback" />
+      </nav>
+    </>
+  );
 }
